d70d5de0696b580a670b69860492863acd324aa5
Hobson Lane <hobson@talentpair.com>
2016-10-26 13:45:57 -0700

    initial copy of NLP_in_Action repo, including docs notes/drafts

---
+++ nlpia/skeleton.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This is a skeleton file that can serve as a starting point for a Python
+console script. To run this script uncomment the following line in the
+entry_points section in setup.cfg:
+
+    console_scripts =
+     fibonacci = NLP_in_Action.skeleton:run
+
+Then run `python setup.py install` which will install the command `fibonacci`
+inside your current environment.
+Besides console scripts, the header (i.e. until _logger...) of this file can
+also be used as template for Python modules.
+
+Note: This skeleton file can be safely removed if not needed!
+"""
+from __future__ import division, print_function, absolute_import
+
+import argparse
+import sys
+import logging
+
+from nlp_in_action import __version__
+
+__author__ = "Hobson Lane"
+__copyright__ = "Hobson Lane"
+__license__ = "mit"
+
+_logger = logging.getLogger(__name__)
+
+
+def fib(n):
+    """
+    Fibonacci example function
+
+    :param n: integer
+    :return: n-th Fibonacci number
+    """
+    assert n > 0
+    a, b = 1, 1
+    for i in range(n-1):
+        a, b = b, a+b
+    return a
+
+
+def parse_args(args):
+    """
+    Parse command line parameters
+
+    :param args: command line parameters as list of strings
+    :return: command line parameters as :obj:`argparse.Namespace`
+    """
+    parser = argparse.ArgumentParser(
+        description="Just a Fibonnaci demonstration")
+    parser.add_argument(
+        '--version',
+        action='version',
+        version='NLP_in_Action {ver}'.format(ver=__version__))
+    parser.add_argument(
+        dest="n",
+        help="n-th Fibonacci number",
+        type=int,
+        metavar="INT")
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action='store_const',
+        const=logging.INFO)
+    parser.add_argument(
+        '-vv',
+        '--very-verbose',
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action='store_const',
+        const=logging.DEBUG)
+    return parser.parse_args(args)
+
+
+def main(args):
+    args = parse_args(args)
+    logging.basicConfig(level=args.loglevel, stream=sys.stdout)
+    _logger.debug("Starting crazy calculations...")
+    print("The {}-th Fibonacci number is {}".format(args.n, fib(args.n)))
+    _logger.info("Script ends here")
+
+
+def run():
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    run()
