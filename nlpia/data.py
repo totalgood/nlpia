12f0def74d8dd4824dc6347dca981cedfd62ab5d
Hobson Lane <github@totalgood.com>
2017-04-22 15:21:53 -0700

    new requirements and data

--- nlpia/data.py
+++ nlpia/data.py
@@ -1,6 +1,14 @@
 import os
 
+# from pugnlp import mkdir_p
+
+import urllib
+
+USER_HOME = os.path.expanduser("~")
 DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
+DATA_URL = 'http://totalgood.org/static/data'
+W2V_FILE = 'GoogleNews-vectors-negative300.bin.gz'
+W2V_URL = DATA_URL.rstrip('/') + '/' + W2V_FILE
 
 with open(os.path.join(DATA_PATH, 'kite.txt')) as f:
     kite_text = f.read()
@@ -11,3 +19,9 @@ with open(os.path.join(DATA_PATH, 'kite_history.txt')) as f:
 harry_docs = ["The faster Harry got to the store, the faster and faster Harry would get home."]
 harry_docs += ["Harry is hairy and faster than Jill."]
 harry_docs += ["Jill is not as hairy as Harry."]
+
+
+def download(name=None):
+    name = (name or '').lower()
+    if name in ('', 'w2v', 'word2vec'):
+        urllib.urlretrieve(W2V_URL, W2V_FILE)
