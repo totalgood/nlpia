b5a69b006b4692c8471754c497f3a01d4f5151a5
Hobson Lane <github@totalgood.com>
2017-04-22 16:02:29 -0700

    w2v url download using requests

--- nlpia/data.py
+++ nlpia/data.py
@@ -1,14 +1,14 @@
 import os
+import requests
 
 # from pugnlp import mkdir_p
-
-import urllib
 
 USER_HOME = os.path.expanduser("~")
 DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
 DATA_URL = 'http://totalgood.org/static/data'
 W2V_FILE = 'GoogleNews-vectors-negative300.bin.gz'
 W2V_URL = DATA_URL.rstrip('/') + '/' + W2V_FILE
+W2V_PATH = os.path.join(DATA_PATH, W2V_FILE)
 
 with open(os.path.join(DATA_PATH, 'kite.txt')) as f:
     kite_text = f.read()
@@ -23,5 +23,11 @@ harry_docs += ["Jill is not as hairy as Harry."]
 
 def download(name=None):
     name = (name or '').lower()
+    file_paths = {}
     if name in ('', 'w2v', 'word2vec'):
-        urllib.urlretrieve(W2V_URL, W2V_FILE)
+        req = requests.get(W2V_URL)
+        with open(W2V_FILE, 'wb') as f:
+            for chunk in req.iter_content(100000):
+                f.write(chunk)
+            file_paths['w2v'] = f.name
+    return file_paths
