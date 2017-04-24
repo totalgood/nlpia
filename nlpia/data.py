3f9be492af6262acd32117edf3dba4a28ce948b7
Hobson Lane <github@totalgood.com>
2017-04-23 21:18:48 -0700

    print instead of logging

--- nlpia/data.py
+++ nlpia/data.py
@@ -55,8 +55,8 @@ def download_file(url, local_file_path=None, chunk_size=1024, verbose=True):
 
     r = requests.get(url, stream=True)
     size = r.headers.get('Content-Length', None)
-    logger.info(r.headers.keys())
-    logger.info('size: {}'.format(size))
+    print(r.headers.keys())
+    print('size: {}'.format(size))
     with open(local_file_path, 'wb') as f:
         for chunk in tqdm_prog(r.iter_content(chunk_size=chunk_size),
                                total=size if size is None else int(1 + 2. * size / chunk_size)):
