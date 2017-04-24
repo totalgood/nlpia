e45a9916f87910949763546491d4c32dd59cac9a
Hobson Lane <github@totalgood.com>
2017-04-24 14:07:04 -0700

    auto index csv

--- nlpia/data.py
+++ nlpia/data.py
@@ -4,13 +4,11 @@ import requests
 from .constants import logging
 
 from pugnlp.futil import path_status
+import pandas as pd
 
-# import time
-
-from tqdm import tqdm
+np = pd.np
 logger = logging.getLogger(__name__)
 
-# from pugnlp import mkdir_p
 
 USER_HOME = os.path.expanduser("~")
 DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
@@ -51,11 +49,11 @@ def download_file(url, local_file_path=None, size=None, chunk_size=1024, verbose
     local_file_path = os.path.join(DATA_PATH, url.split('/')[-1]) if local_file_path is None else local_file_path
     if not (local_file_path.startswith(DATA_PATH) or local_file_path[0] in ('/', '~')):
         local_file_path = os.path.join(DATA_PATH, local_file_path)
-    if verbose:
-        tqdm_prog = tqdm
-        print('requesting URL: {}'.format(W2V_URL))
-    else:
-        tqdm_prog = no_tqdm
+    # if verbose:
+    #     tqdm_prog = tqdm
+    #     print('requesting URL: {}'.format(W2V_URL))
+    # else:
+    #     tqdm_prog = no_tqdm
     stat = path_status(local_file_path)
     if stat['type'] == 'file' and stat['size'] == size:  # TODO: check md5
         return local_file_path
@@ -71,3 +69,27 @@ def download_file(url, local_file_path=None, size=None, chunk_size=1024, verbose
         #     if chunk:  # filter out keep-alive chunks
         #         f.write(chunk)
     return local_file_path
+
+
+def read_csv(*args, **kwargs):
+    """Like pandas.read_csv, only little smarter (checks first column to see if it should be the data frame index)"""
+    index_names = ('Unnamed: 0',)
+    kwargs.update({'low_memory': False})
+    df = pd.read_csv(*args, **kwargs)
+    if df.columns[0] in index_names or (df[df.columns[0]] == df.index).all():
+        df = df.set_index(df.columns[0], drop=True)
+    elif (df[df.columns[0]] == np.arange(len(df))).all():
+        df = df.set_index(df.columns[0], drop=False)
+    elif (df.index == np.arange(len(df))).all() and str(df[df.columns[0]].dtype).startswith('int') and df[df.columns[0]].count() == len(df):
+        df = df.set_index(df.columns[0], drop=False)
+    return df
+
+
+def multifile_dataframe(paths=['urbanslang{}of4.csv'.format(i) for i in range(1, 5)], header=0, index_col=None):
+    """Like pandas.read_csv, but loads and concatenates (df.append(df)s) DataFrames together"""
+    df = pd.DataFrame()
+    for p in paths:
+        df = df.append(read_csv(p, header=header, index_col=index_col), ignore_index=True if not index_col else False)
+    if index_col and df.index.name == index_col:
+        del df[index_col]
+    return df
