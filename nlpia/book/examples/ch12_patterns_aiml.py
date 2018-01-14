9691ec87007d58ebe1429e2149a54c3ed9dc5e34
Hobson Lane <github@totalgood.com>
2018-01-13 16:35:49 -0800

    voice recognition and aiml for ch12 in separate py files

---
+++ nlpia/book/examples/ch12_patterns_aiml.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Natural Language Processing in Action -- Chapter 12 Getting Chatty -- examples
+>>> import os
+>>> from nlpia.constants import DATA_PATH
+>>> import aiml_bot
+>>> bot.learn(os.path.join(DATA_PATH, 'greeting_step1.aiml'))
+Loading /Users/hobs/src/nlpia/nlpia/data/greeting_step1.aiml...
+done (0.00 seconds)
+
+>>> bot.respond('Hello Rosa')
+'Hi there!'
+
+>>> bot.respond("hello **stupid** !!!")
+'Good one, human.'
+"""
+
+import os
+from nlpia.constants import DATA_PATH
+import aiml_bot
+
+
+""" AIML Step 1
+<category><pattern>HELLO ROSA </pattern><template>Hi Human!</template></category>
+<category><pattern>HELLO STUPID </pattern><template>Good one, human.</template></category>
+"""
+bot = aiml_bot.Bot(learn=os.path.join(DATA_PATH, 'greeting_step1.aiml'))
+# Loading /Users/hobs/src/nlpia/nlpia/data/greeting_step1.aiml...
+# done (0.00 seconds)
+# Loading /Users/hobs/src/nlpia/nlpia/data/greeting_step1.aiml...
+# done (0.00 seconds)
+
+""" AIML Step1: Good ones """
+bot.respond("Hello Rosa,")
+# 'Hi there!'
+bot.respond("hello **stupid** !!!")
+# 'Good one, human.'
+
+""" AIML Step1: Mismatches """
+bot.respond("Helo Rosa")
+# WARNING: No match found for input: Helo Rosa
+# ''
+bot.respond("Hello stu-pid")
+# WARNING: No match found for input: Hello stu-pid
+# Out[4]: ''
+
+
+bot = aiml_bot.Bot(learn=os.path.join(DATA_PATH, 'greeting_step2.aiml'))
