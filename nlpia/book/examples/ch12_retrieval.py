4abf6356aec8ce0b3dba903607ecd176bcbf4127
Hobson Lane <github@totalgood.com>
2018-01-14 22:05:35 -0800

    working on ch12 retreival bot

---
+++ nlpia/book/examples/ch12_retrieval.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Natural Language Processing in Action -- Chapter 12 Getting Chatty -- 3. Retrieval (Search) section
+"""
+import os
+from nlpia.constants import DATA_PATH
+import aiml_bot
+
+
+def split_turns(s, splitter=re.compile('__eot__')):
+    for utterance in splitter.split(s):
+        utterance = utterance.replace('__eou__', '\n')
+        utterance = utterance.replace('__eot__', '')
+        if len(utterance.strip()):
+            yield utterance
+
+
+for i, record in df.head(4).iterrows():
+    statement = list(split_turns(record.Context))[-1]  # <1>
+    reply = list(split_turns(record.Utterance))[-1]  # <2>
+    print('Statement: {}'.format(statement))
+    print('Reply: {}'.format(reply))
+# <1> We need to use `list` to force iteration through the generator
+# <2> The `[-1]` index retrievs the last "turn" in the sequence, discarding everything else
+
+
+""" AIML Step 1
+<category><pattern>HELLO ROSA </pattern><template>Hi Human!</template></category>
+<category><pattern>HELLO TROLL </pattern><template>Good one, human.</template></category>
+"""
+bot = aiml_bot.Bot(learn=os.path.join(DATA_PATH, 'greeting_step1.aiml'))
+# Loading /Users/hobs/src/nlpia/nlpia/data/greeting_step1.aiml...
+# done (0.00 seconds)
+# Loading /Users/hobs/src/nlpia/nlpia/data/greeting_step1.aiml...
+# done (0.00 seconds)
+
+""" AIML Patterns Step1: Good ones """
+bot.respond("Hello Rosa,")
+# 'Hi there!'
+bot.respond("hello **troll** !!!")
+# 'Good one, human.'
+
+""" AIML Patterns Step1: Mismatches """
+bot.respond("Helo Rosa")
+# WARNING: No match found for input: Helo Rosa
+# ''
+bot.respond("Hello t-r-o-l-l")
+# WARNING: No match found for input: Hello t-r-o-l-l
+# Out[4]: ''
+
+""" AIML Patterns Step2: Synonyms """
+bot.learn(os.path.join(DATA_PATH, 'greeting_step2.aiml'))
+bot.respond("Hey Rosa")
+'Hi there!'
+bot.respond("Hi Rosa")
+'Hi there!'
+bot.respond("Helo Rosa")
+'Hi there!'
+bot.respond("hello **troll** !!!")  # <1>
+'Good one, human.'
+
+""" AIML Patterns Step2: Mismatches """
+bot.respond("Hello t-r-o-l-l")
+# WARNING: No match found for input: Hello t-r-o-l-l
+# Out[4]: ''
+
+""" AIML Patterns Step3: Random Responses and Lists """
+bot = aiml_bot.Bot(learn=os.path.join(DATA_PATH, 'greeting_step3.aiml'))
+bot.learn(os.path.join(DATA_PATH, 'greeting_step3.aiml'))
+bot.respond("Hey Rosa")
+'Hello friend'
+bot.respond("Hey Rosa")
+'Hey you :)'
+bot.respond("Hey Rosa")
+'Hi Human!'
