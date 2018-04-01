5897d9b41690246d13ed8992d9131dfd7af2b3e6
Hobson Lane <hobson@users.noreply.github.com>
2018-03-31 17:15:40 -0700

    update audio.py

--- src/nlpia/scripts/audio.py
+++ src/nlpia/scripts/audio.py
@@ -4,10 +4,15 @@ from __future__ import absolute_import, division, print_function
 from timeit import default_timer as timer
 import os
 from traceback import format_exc
+import argparse
+# from io import BytesIO
 
-import pyaudio
 
-# from nlpia.utils import 
+# import pyaudio  # only works with python2.7?
+import simpleaudio as sa
+# import wave
+import speech_recognition as sr
+import pyttsx3
 
 
 def record_audio(source='Microphone', energy_threshold=300, pause_threshold=.9,
@@ -35,31 +40,42 @@ def record_audio(source='Microphone', energy_threshold=300, pause_threshold=.9,
     return audio
 
 
-def play_audio(audio, start=0, stop=None, save=None, batch_size=1024):
-    player = pyaudio.PyAudio()
-    if isinstance(audio, str):
-        input_stream = wave.openfp(open(audio, 'rb'))
-    else:
-        input_stream = wave.openfp(BytesIO(audio.get_wav_data()))
+# def play_pyaudio(audio, start=0, stop=None, save=None, batch_size=1024):
+#     player = pyaudio.PyAudio()
+#     if isinstance(audio, str):
+#         input_stream = wave.openfp(open(audio, 'rb'))
+#     else:
+#         input_stream = wave.openfp(BytesIO(audio.get_wav_data()))
 
-    output_stream = player.open(
-        format=player.get_format_from_width(input_stream.getsampwidth()),
-        channels=input_stream.getnchannels(),
-        rate=input_stream.getframerate(),
-        output=True)
-    # play stream
-    batch = input_stream.readframes(batch_size)
-    save = open(save, 'wb') if save is not None else save
-    i = 0
-    while batch and (i + 1) * batch_size >= start and (stop is None or (i - 1) * batch_size < stop):
-        if stop is not None and i * batch_size >= stop:
-            batch = batch[:(stop % batch_size)]
-        if start and i * batch_size < start:
-            batch = batch[(start % batch_size):]
-        output_stream.write(batch)
-        if save is not None:
-            save.write(batch)
-        batch = input_stream.readframes(batch_size)
+#     output_stream = player.open(
+#         format=player.get_format_from_width(input_stream.getsampwidth()),
+#         channels=input_stream.getnchannels(),
+#         rate=input_stream.getframerate(),
+#         output=True)
+#     # play stream
+#     batch = input_stream.readframes(batch_size)
+#     save = open(save, 'wb') if save is not None else save
+#     i = 0
+#     while batch and (i + 1) * batch_size >= start and (stop is None or (i - 1) * batch_size < stop):
+#         if stop is not None and i * batch_size >= stop:
+#             batch = batch[:(stop % batch_size)]
+#         if start and i * batch_size < start:
+#             batch = batch[(start % batch_size):]
+#         output_stream.write(batch)
+#         if save is not None:
+#             save.write(batch)
+#         batch = input_stream.readframes(batch_size)
+#     return audio
+
+
+def play_audio(audio, start=0, stop=None, save=None, batch_size=1024):
+    if isinstance(audio, str):
+        # wave_stream = wave.openfp(open(audio, 'rb'))
+        wave_obj = sa.WaveObject.from_wave_file(audio)
+    else:
+        wave_obj = sa.WaveObject.from_wave_read(audio)
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
     return audio
 
 
@@ -196,7 +212,7 @@ def main():
         save_audio(audio, filepath)
         print("Saved audio clip to '{}'".format(filepath))
         print("Playing audio clip ...")
-        play_audio(audio)
+        play_audio(filepath)
         print("Done.")
 
     if args.playpath:
