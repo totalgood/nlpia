74aafdb5115842a274b66e77c4494f389a39e056
Hobson Lane <github@totalgood.com>
2018-07-22 19:19:31 -0700

    delete debug prints

--- src/nlpia/book_parser.py
+++ src/nlpia/book_parser.py
@@ -1,5 +1,6 @@
-import glob
 import os
+import sys
+import glob
 
 
 def get_lines(file_path):
@@ -8,21 +9,21 @@ def get_lines(file_path):
         file_path  string  aboslute path of the Manning manuscript asciidoc files
                            i.e.: /Users/cole-home/repos/nlpinaction/manuscript
 
-    returns:
-        list of strings
+    Returns:
+        list of lists of str, one list for each Chapter or Appendix
     '''
     path = os.path.join(file_path, 'Chapter*')
     files = glob.glob(path)
     lines = []
     for file in files:
         with open(file, 'r') as f:
-            lines += f.readlines()
+            lines.append(f.readlines())
 
     path = os.path.join(file_path, 'Appendix*')
     files = glob.glob(path)
     for file in files:
         with open(file, 'r') as f:
-            lines += f.readlines()
+            lines.append(f.readlines())
     return lines
 
 
@@ -67,7 +68,6 @@ def tag_lines(lines):
     tup_lines = []
     for idx, line in enumerate(lines):
         normalized_line = line.lower().strip().replace(" ", "")
-        print(normalized_line)
 
         if not normalized_line:
             tag = 'blank_line'
@@ -84,7 +84,6 @@ def tag_lines(lines):
             open_block = True
             tag = 'block_header'
         elif open_block and idx == block_start + 1:
-            print('code---block---')
             if not normalized_line.startswith('--') and not normalized_line.startswith('=='):
                 block_terminator = '\n'
             else:
@@ -112,3 +111,18 @@ def tag_lines(lines):
         tup_lines.append((tag, line.strip()))
 
     return tup_lines
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    book_dir = os.path.curdir
+    if args:
+        book_dir = args[0]
+    include_tags = ['natural']
+    if len(args) > 1:
+        include_tags = args[1:]
+
+    for section in get_lines(book_dir):
+        for line in tag_lines(section):
+            if line[0] in include_tags:
+                print(line[1])
