8bd30ecd790a0906a5221c30b3d2a04b56131fcc
Hobson Lane <github@totalgood.com>
2018-08-14 13:50:26 -0700

    fix plot

--- src/nlpia/book/examples/ch03_zipf.py
+++ src/nlpia/book/examples/ch03_zipf.py
@@ -4,17 +4,19 @@
 >>> axes = rankpop.plot(style='o', logx=True, logy=True)
 >>> xlabel = plt.xlabel('US City Rank in Population')
 >>> ylabel = plt.ylabel('US City Population')
->>> title plt.title('Counting People in Cities Like Counting Words in Documents')
+>>> plt.title('Counting People in Cities Like Counting Words in Documents')
 >>> plt.tight_layout()
 >>> plt.show()
 """
 import pandas as pd
+import seaborn
+from matplotlib import pyplot as plt
 
 df = pd.read_html('https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population', header=0)[3]
 rankpop = df[[df.columns[0], df.columns[3]]]
 axes = rankpop.plot(style='o', logx=True, logy=True)
 xlabel = plt.xlabel('US City Rank in Population')
 ylabel = plt.ylabel('US City Population')
-title plt.title('Counting People in Cities Like Counting Words in Documents')
+plt.title('Counting People in Cities Like Counting Words in Documents')
 plt.tight_layout()
 plt.show()
