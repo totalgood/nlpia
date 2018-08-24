d8208d68ae2cbc050745b5edae1a2cc4707b7203
Hobson Lane <github@totalgood.com>
2018-08-24 13:26:41 -0700

    dont test skeleton and fix .travis.yml

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    Dummy conftest.py for NLP_in_Action.
-
-    If you don't know what this is for, just leave it empty.
     Read more about conftest.py under:
     https://pytest.org/latest/plugins.html
 """
