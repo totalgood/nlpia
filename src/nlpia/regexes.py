f7274462231e88adf4e445a43d01ad5ad0bf97d0
Hobson Lane <github@totalgood.com>
2018-09-03 14:36:52 -0700

    upgrade pugnlp version requirements

--- src/nlpia/regexes.py
+++ src/nlpia/regexes.py
@@ -6,8 +6,12 @@
 >>> CRE_ACRONYM.findall('National science Foundation (NSF)')
 [('', '', '', '', 'National science Foundation', 'N', 's', 'F', 'NSF')]
 """
-from nlpia.constants import logging
+from nlpia.constants import logging, DATA_PATH
 import re
+import os
+import copy
+
+from pugnlp.regexes import *  # noqa
 
 
 logger = logging.getLogger(__name__)
@@ -15,3 +19,14 @@ logger = logging.getLogger(__name__)
 RE_ACRONYM2 = r'((\w)[\w0-9]{2,16}[ ](\w)[\w0-9]{2,16})[ ]\((\2\3)\)'
 RE_ACRONYM3 = r'((\w)[\w0-9]{2,16}[ ](\w)[\w0-9]{2,16}[ ](\w)[\w0-9]{2,16})[ ]\((\6\7\8)\)'
 CRE_ACRONYM = re.compile(RE_ACRONYM2 + '|' + RE_ACRONYM3, re.IGNORECASE)
+
+
+def to_tsv():
+    """ Save all regular expressions to a tsv file so they can be more easily copy/pasted in Sublime """
+    with open(os.path.join(DATA_PATH, 'regexes.tsv'), mode='wt') as fout:
+        vars = copy.copy(tuple(globals().items()))
+        for k, v in vars:
+            if k.lower().startswith('cre_'):
+                fout.write(k[4:] + '\t' + v.pattern + '\n')
+            elif k.lower().startswith('re_'):
+                fout.write(k[3:] + '\t' + v.pattern + '\n')
