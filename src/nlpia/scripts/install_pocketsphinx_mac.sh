1aee46cf61df9ee02383c48a0d234d20e9e616cf
Hobson Lane <hobs@totalgood.com>
2018-12-20 18:10:40 -0800

    ignore all h5, add script for installing pocketsphinx on mac, add conda environment for pocketsphinx called "assist"

---
+++ src/nlpia/scripts/install_pocketsphinx_mac.sh
@@ -0,0 +1,22 @@
+conda install pocketsphinx
+conda install swig
+brew install --HEAD watsonbox/cmu-sphinx/cmu-sphinxbase
+conda install pocketsphinx-python
+brew install --HEAD OpenAL
+brew install OpenAL
+xcode-select --install # Install Command Line Tools if you haven't already.
+sudo xcode-select --switch /Library/Developer/CommandLineTools # Enable command line tools
+brew install --HEAD watsonbox/cmu-sphinx/cmu-sphinxbase
+brew install --HEAD watsonbox/cmu-sphinx/cmu-pocketsphinx
+brew install cvs
+brew install openal-soft
+xcode install openal-soft
+brew install openal-soft
+brew install pocketsphinx-python
+conda install pyaudio3
+conda install pyaudio
+brew install snips
+conda install swig
+conda install pyaudio
+conda install pocketsphinx
+conda install pocketsphinx-python
