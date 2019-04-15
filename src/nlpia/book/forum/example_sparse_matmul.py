d2b4f147fd0617a76201a8b5ac90e96c2bb84d88
hobs <hobs+github@totalgood.com>
2019-04-15 12:13:42 -0700

    tabs to spaces in examples hunspell2json etc

--- src/nlpia/book/forum/example_sparse_matmul.py
+++ src/nlpia/book/forum/example_sparse_matmul.py
@@ -24,10 +24,10 @@ A = scipy.sparse.csr_matrix([[1, 2, 0], [0, 0, 3], [4, 0, 5]])
 v = scipy.sparse.csr_matrix([[1], [0], [-1]])
 A.dot(v)
 # <3x1 sparse matrix of type '<class 'numpy.int64'>'
-# 	with 3 stored elements in Compressed Sparse Row format>
+#     with 3 stored elements in Compressed Sparse Row format>
 scipy.dot(A, v)
 # <3x1 sparse matrix of type '<class 'numpy.int64'>'
-# 	with 3 stored elements in Compressed Sparse Row format>
+#     with 3 stored elements in Compressed Sparse Row format>
 np.dot(A, v)
 # <3x1 sparse matrix of type '<class 'numpy.int64'>'
-# 	with 3 stored elements in Compressed Sparse Row format>
+#     with 3 stored elements in Compressed Sparse Row format>
