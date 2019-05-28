daa6402f86775d8481bdc75d314cacb49f43669c
hobs <hobs+github@totalgood.com>
2019-05-28 09:47:43 -0700

    cosmetics

--- src/nlpia/book/examples/ch10_english_spanish_translator_v2.py
+++ src/nlpia/book/examples/ch10_english_spanish_translator_v2.py
@@ -1,10 +1,10 @@
-import os
-import re
+# import os
+# import re
 import sys
 
-import pandas as pd
+# import pandas as pd
 import numpy as np
-from tqdm import tqdm
+# from tqdm import tqdm
 from nlpia.loaders import get_data
 
 if len(sys.argv) > 1:
@@ -13,6 +13,7 @@ else:
     lang = 'spa'
 
 source_lang = 'eng'
+max_text_len = 32  # 32: 62,829 spa examples (2 min/epoch), 64: 100k spa examples (6 min/epoch)
 
 
 df = get_data(lang)
@@ -24,6 +25,10 @@ if lang not in df.columns:
 
 df.columns = [source_lang, lang]
 df = df.dropna()
+df[source_lang + '_len'] = df[source_lang].str.len()
+df = df[df[source_lang + '_len'] < max_text_len]
+# need to make sure all batches are composed of similar-length texts (see NLPIA ch 10 & ch 13)
+
 input_texts, target_texts = [], []  # <1>
 start_token, stop_token = '\t\n'  # <3>
 input_vocab = set()  # <2>
@@ -68,11 +73,15 @@ for i, (input_text, target_text) in enumerate(
     decoder_input_onehot[i, np.arange(len(target_text)), k] = 1.
     decoder_target_onehot[i, np.arange(len(target_text) - 1), k[1:]] = 1.
 # <1> You use numpy for the matrix manipulations.
-# <2> The training tensors are initialized as zero tensors with the shape of number of samples (this number should be equal for the input and target samples) times the maximum number of sequence tokens times the number of possible characters.
+# <2> The training tensors are initialized as zero tensors with the shape of number of samples
+#     (this number should be equal for the input and target samples) times the maximum number of sequence tokens
+#     times the number of possible characters.
 # <3> Loop over the training samples; input and target texts need to match.
 # <4> Loop over each character of each sample.
-# <5> Set the index for the character at each time step to one; all other indices remain at zero. This creates the one-hot encoded representation of the training samples.
-# <6> For the training data for the decoder, you create the `decoder_input_data` and `decoder_target_data` (which is one time step behind the _decoder_input_data_).
+# <5> Set the index for the character at each time step to one; all other indices remain at zero.
+#     This creates the one-hot encoded representation of the training samples.
+# <6> For the training data for the decoder, you create the `decoder_input_data` and `decoder_target_data`
+#     (which is one time step behind the _decoder_input_data_).
 
 
 """Construct and train a character sequence encoder-decoder network
@@ -183,8 +192,7 @@ def decode_sequence(input_seq):
     thought = encoder_model.predict(input_seq)  # <1>
 
     target_seq = np.zeros((1, 1, len(output_vocab)))  # <2>
-    target_seq[0, 0, output_vocab.index(stop_token)
-        ] = 1.  # <3>
+    target_seq[0, 0, output_vocab.index(stop_token)] = 1.  # <3>
     stop_condition = False
     generated_sequence = ''
 
@@ -192,6 +200,7 @@ def decode_sequence(input_seq):
         output_tokens, h, c = decoder_model.predict(
             [target_seq] + thought) # <4>
 
+        # take only the most likely character (temperature = 0)
         generated_token_idx = np.argmax(output_tokens[0, -1, :])
         generated_char = output_vocab[generated_token_idx]
         generated_sequence += generated_char
@@ -207,7 +216,7 @@ def decode_sequence(input_seq):
     return generated_sequence
 
 
-def respond(input_text):
+def respond(input_text, input_vocab=input_vocab):
     input_text = input_text.lower()
     input_text = ''.join(c if c in input_vocab else ' ' for c in input_text)
     input_seq = np.zeros((1, max_encoder_seq_len, len(input_vocab)), dtype='float32')
@@ -217,90 +226,3 @@ def respond(input_text):
     print('Human: {}'.format(input_text))
     print('Bot:', decoded_sentence)
     return decoded_sentence
-
-"""
-respond('Hi Rosa, how are you?')
-respond('Hi Jim, how are you?')
-respond('Hi Barak, how are you?')
-respond('Hi Amy, how are you?')
-respond('Hi Paris, how are you?')
-respond('Hi Joe, how are you?')
-respond('Hi Jane, how are you?')
-respond('Hey Jane, how are you?')
-respond('Hey Jon, how are you?')
-respond('Hey John, how are you?')
-respond('Hey Joe, how are you?')
-respond('Hey Jim, how are you?')
-respond('Hey Ashley, how are you?')
-respond('Hey my love, how are you?')
-respond('Hey Arzu, how are you?')
-respond("I'm talking about us.")
-respond("What are you trying to say?")
-In [38]: respond('Hi Rosa, how are you?')
-Human: hi rosa, how are you?
-Bot: hello, he was the one when you wanted to see you again.
-
-In [39]: respond('Hi Jim, how are you?')
-Human: hi jim, how are you?
-Bot: how are you?
-
-In [40]: respond('Hi Barak, how are you?')
-Human: hi barak, how are you?
-Bot: hello, he was the one when i get the best way to get the show off the back.
-
-In [41]: respond('Hi Amy, how are you?')
-Human: hi amy, how are you?
-Bot: hello, man. i was a second.
-
-In [42]: respond('Hi Paris, how are you?')
-Human: hi paris, how are you?
-Bot: his heart studs the fucking chark off.
-
-In [43]: respond('Hi Joe, how are you?')
-Human: hi joe, how are you?
-Bot: his his hate is beautiful on the way i was the one who wanted to say that.
-
-In [44]: respond('Hi Jane, how are you?')
-Human: hi jane, how are you?
-Bot: hello, hello, martine. i got a second. i'm a fine boy from the ship.
-
-In [45]: respond('Hey Jane, how are you?')                                                                                        [870/1906]
-Human: hey jane, how are you?
-Bot: hello, mr. decker. what do you mean what happened?
-
-In [46]: respond('Hey Jon, how are you?')
-Human: hey jon, how are you?
-Bot: hello, mr. decker. what do you mean what happened?
-
-In [47]: respond('Hey John, how are you?')
-Human: hey john, how are you?
-Bot: hello, mr. decker. what do you mean what happened?
-
-In [48]: respond('Hey Joe, how are you?')
-Human: hey joe, how are you?
-Bot: hello, mr. decker. what do you mean what happened?
-
-In [49]: respond('Hey Jim, how are you?')
-Human: hey jim, how are you?
-Bot: how much money i want to say that?
-
-In [50]: respond('Hey Ashley, how are you?')
-Human: hey ashley, how are you?
-Bot: his his morning.
-
-In [51]: respond('Hey my love, how are you?')
-Human: hey my love, how are you?
-Bot: here. i was just thinking about it.
-
-In [52]: respond('Hey Arzu, how are you?')
-Human: hey arzu, how are you?
-Bot: hi. what are you talking about?
-
-In [53]: respond("I'm talking about us.")
-Human: i'm talking about us.
-Bot: i know.
-
-In [54]: respond("What are you trying to say?")
-Human: what are you trying to say?
-Bot: i don't know.
-"""
