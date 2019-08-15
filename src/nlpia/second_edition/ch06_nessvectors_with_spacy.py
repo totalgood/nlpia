d1b43967f20c8ccdc839d5093dbf12780e608569
hobs <hobs+github@totalgood.com>
2019-08-15 07:37:44 -0700

    fix spacy indexer

--- src/nlpia/second_edition/ch06_nessvectors_with_spacy.py
+++ src/nlpia/second_edition/ch06_nessvectors_with_spacy.py
@@ -5,12 +5,12 @@ import json
 import spacy
 import annoy
 
+# "en_core_web_lg" and "en_core_web_md"
 # English multi-task CNN trained on OntoNotes and Common Crawl (GLoVe).
 # GloVe vectors, context-specific token vectors, POS, dependency tree, NER
 # _md: 91MB, 20k 300D GloVe vectors
-# spacy.download("en_core_web_md")
 # _lg: 788MB, 685k 300D GloVe vectors, 96.6% NER
-spacy.download("en_core_web_lg")
+# !python -m spacy download "en_core_web_lg"
 nlp = spacy.load("en_core_web_lg")
 
 WV_DIM = len(nlp('word').vector)
@@ -19,13 +19,14 @@ N_TREES = WV_DIM * 1  # annoy recommends *2 but doesn't recommend DIM > 100
 
 def build_index(vectors, index=None, n_trees=None, metric='euclidean'):
     """ metrics: euclidian, dot, angular """
-    for v in vectors:
+    vector_iterator = iter(vectors)
+    for v in vector_iterator:
         n_dim = len(v)
         annindex = annoy.AnnoyIndex(n_dim, metric=metric)
-        annindex.add_item(v)
+        annindex.add_item(0, v)
         break
-    for v in vectors:
-        annindex.add_item(v)
+    for i, v in enumerate(vector_iterator):
+        annindex.add_item(i + 1, v)
         break
     annindex.build(n_trees=n_dim * 2 if n_trees is None else n_trees)
     return annindex
